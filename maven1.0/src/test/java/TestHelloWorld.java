a11026801ba90102e998b8d0757a630564a4e24f
mlavanya13 <lavanya.mandalapu@gmail.com>
2020-06-22 15:37:18 -0700

    modified and adding to branchx

--- maven1.0/src/test/java/TestHelloWorld.java
+++ maven1.0/src/test/java/TestHelloWorld.java
@@ -3,7 +3,7 @@ import org.testng.annotations.Test;
 public class TestHelloWorld {
 	@Test
 	public void testHelloworld() {
-	       System.out.println("MY FIRST TEST")
+	       System.out.println("MY FIRST TEST");
 	       System.out.println("HELLOWORLD");
 	}
 
