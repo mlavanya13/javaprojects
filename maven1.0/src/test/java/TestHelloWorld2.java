d39eca98f1b5e9267458b71bded938f3fc24b4ff
mlavanya13 <lavanya.mandalapu@gmail.com>
2020-06-22 15:34:29 -0700

    adding to branchX

---
+++ maven1.0/src/test/java/TestHelloWorld2.java
@@ -0,0 +1,10 @@
+import org.testng.annotations.Test;
+
+public class TestHelloWorld2 {
+	@Test
+	public void testHelloworld() {
+	       System.out.println("MY FIRST TEST");
+	       System.out.println("HELLOWORLD");
+	}
+
+}
